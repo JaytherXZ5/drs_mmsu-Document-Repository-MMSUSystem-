e298bf671b19062fdc8be1b9df96f1a3e1d24934
JaytherJann <jaytherjanbuaay2.0@gmail.com>
2023-12-04 04:29:04 +0800

    first commit

--- resources/js/Components/UploadFileModal.vue
+++ resources/js/Components/UploadFileModal.vue
@@ -1,6 +1,6 @@
 <template>
-    <div :class="{'hidden': !upload_modalActive, 'absolute w-screen h-screen top-0 left-0 bg-gray-900 z-50 bg-opacity-30 flex justify-center items-start p-40': upload_modalActive}">
-        <div class="bg-white w-[50%] h-[45%] rounded-lg z-50">
+    <div :class="{'hidden': !upload_modalActive, 'absolute w-screen h-screen top-0 left-0 bg-gray-900 z-50 bg-opacity-30 flex justify-center items-start p-20': upload_modalActive}">
+        <div class="bg-white w-[50%] h-[62%] rounded-lg z-50">
                 <transition
                 enter-active-class="transition duration-500 ease-out"
                 enter-from-class="transform scale-95 opacity-0"
@@ -16,19 +16,32 @@
                             <slot/> 
                             <form @submit="uploadFile">
                             <div class="modal-content flex flex-col px-4">
-                                <h1 class="px-2 pt-6 font-poppins text- text-green-800 ">Upload a File</h1>
-                                
-                                <input type="file" @change="handleFileChange" ref="uploadFile" multiple>
-                               
-                                <div @click="openDialog" class="">
+                                <h1 class="px-2 pt-6 font-montserrat text- text-gray-800 ">UPLOAD FILES</h1>
+                                <div class="mt-2">
+                                    <!--<input type="text" :value="getFilesName()" readonly placeholder="Choose Files">-->
+                                    <input class="hidden" type="file" @change="handleFileChange" ref="uploadFile" multiple>
+                                    <button @click.prevent="openDialog" class="border-2 shadow-r hover:border-green-600 w-[50%] h-10 p-2 rounded-md flex flex-row items-center gap-4 ">
+                                        <font-awesome-icon :icon="faPaperClip" class=" w-8 border-2  border-yellow-600 rounded-full py-1 my-1 h-full text-green-800 hover:rotate-90 transition-transform duration-300"/>
+                                        <h1 class="font-montserrat text-gray-600 opacity-80 ">CHOOSE FILES</h1>
+                                    </button>
 
                                 </div>
+                                <h1 class="font-montserrat p-2 mt-2 text-gray-600 opacity-70">SELECTED:</h1>
+                                <div  class="rounded-md border overflow-y-auto flex flex-col h-[150px]">
+                                    <div class="border " v-for="file in selectedFiles">
+                                        <h1 class="px-2 py-1 font-montserrat text-gray-600 opacity-80">{{ file.name }}</h1>
+                                    </div>
+                                </div>
+                                
+                                <!--<div @click="openDialog" class="border h-10">
+                                    
+                                </div>-->
                             </div>
                             
                             <div class="px-4 flex w-full justify-end mt-3">
                                 
-                                <button  type="submit" class="ml-2 border-2 w-20 h-10 rounded-lg bg-violet-500 shadow-left-side text-white hover:scale-110  transition-transform duration-300">Done</button>
-                                <button  @click="close" class="ml-2 border-2 w-20 h-10 rounded-lg bg-violet-500 shadow-left-side text-white hover:scale-110  transition-transform duration-300">Close</button>
+                                <button  type="submit" class="ml-2 border-2 w-20 h-10 rounded-lg bg-violet-500 shadow-left-side text-white hover:scale-110  transition-transform duration-300">Upload</button>
+                                <button  @click.prevent="close" class="ml-2 border-2 w-20 h-10 rounded-lg bg-violet-500 shadow-left-side text-white hover:scale-110  transition-transform duration-300">Close</button>
 
                             </div>
                             </form>
@@ -46,56 +59,65 @@
 import {faCircleXmark} from '@fortawesome/free-regular-svg-icons'
 import axios from 'axios';
 import Swal from 'sweetalert2';
+import { faPaperclip } from '@fortawesome/free-solid-svg-icons';
 
 export default{
-    data(){
-        return{
-
-            files:[],
+    data() {
+        return {
+            files: [],
             selectedFiles: [],
-            uploadedFiles:[],
-        }
+            uploadedFiles: [],
+        };
     },
-    methods:{
-        handleFileChange(event){
-            
-                this.selectedFiles =Array.from(event.target.files);
-            
+    methods: {
+        handleFileChange(event) {
+            this.selectedFiles = Array.from(event.target.files);
         },
-
-
-        openDialog(){
+        openDialog() {
             const elem = this.$refs.uploadFile;
             elem.click();
         },
         async uploadFile() {
-      const formData = new FormData();
+            const formData = new FormData();
+            this.selectedFiles.forEach((file) => {
+                formData.append('files[]', file);
+            });
+            try {
+                const response = await axios.post(`/api/uploadFiles/${this.$route.params.id}`, formData);
+                this.uploadedFiles = response.data.files;
+            }
+            catch (error) {
+                Swal.fire(error?.response?.data?.message);
+            }
+        },
+        getFilesName(){
+            let filesName = []
 
-      this.selectedFiles.forEach((file) => {
-        formData.append('files[]', file);
-      });
+            if(this.selectedFiles.length > 0){
+                for(let file of this.selectedFiles){
+                    filesName.push(file.name)
+                }
+            }
 
-      try {
-       
-        const response = await axios.post(`/api/uploadFiles/${this.$route.params.id}`, formData);
-        this.uploadedFiles = response.data.files;
-        
-      } catch (error) {
-        Swal.fire(error?.response?.data?.message);
-      }
+            return filesName.join("")
+        }
     },
-    },
-    
-  
-
     props: ["upload_modalActive"],
-    setup(props, {emit}){
-        const close = () =>{
-
+    setup(props, { emit }) {
+        const close = () => {
             emit('close');
-            
         };
-        return {close}
+        return { close };
     },
+
+    computed:{
+        faPaperClip(){return faPaperclip;},
+    },
+    mounted(){
+        
+    }
+    
+
+
 }
 </script>
